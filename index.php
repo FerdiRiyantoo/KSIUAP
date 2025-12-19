13bdd20f0e663c8cb1d246fd456fda8667c687a5
Ferdi Riyanto <ferdiriyanto00@gmail.com>
2025-12-19 10:26:04 +0700

    ganti Index

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-echo "<h1>Tugas Deployment Kelompok XX</h1>";
-echo "<h3>Anggota Kelompok:</h3>";
-echo "<ul>";
-echo "<li>Nama Anggota 1</li>";
-echo "<li>Nama Anggota 2</li>";
-echo "<li>Nama Anggota 3</li>";
-echo "</ul>";
+echo "<h1>Tugas Ferdi Riyanto</h1>";
 ?>
